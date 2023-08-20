2852acd4ed012c2c92c1622ab0e8a027a8224f53
peebaKU <baramee.254450@gmail.com>
2023-08-20 14:10:48 +0700

    version 2

--- Memory/Memory.c
+++ Memory/Memory.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-
-int numgobal1;
-int numgobal2;
-int numgobal3;
+int numgobal=3;
+int numgobal1=3;
+int numgobal2=3;
+int numgobal3=3;
 
 int ShowStack(int num){
     printf("%d : %p \n",num,&num);   
@@ -15,22 +15,25 @@ int ShowStack(int num){
 }
 
 int ShowHeap(int num){
-    int *a = malloc(3);
-    int *b = malloc(3);
-    int *c = malloc(3);
-    int *d = malloc(3);
+    int *a = malloc(num* sizeof(int));
+    int *b = malloc(numgobal1* sizeof(int));
+    int *c = malloc(numgobal2* sizeof(int));
+    int *d = malloc(numgobal3* sizeof(int));
     printf("d: %p\n",d);
     printf("c: %p\n",c);
     printf("b: %p\n",b);
     printf("a: %p\n",a);
 }
 
-
-
 int main() {
     printf("Stack\n");
-    ShowStack(3);
+    ShowStack(numgobal);
     printf("Heap\n");
-    ShowHeap(3);
+    ShowHeap(numgobal);
+    printf("Data\n");
+    printf("numgobal: %p\n",numgobal);
+    printf("numgobal1: %p\n",numgobal1);
+    printf("numgobal2: %p\n",numgobal2);
+    printf("numgobal3: %p\n",numgobal3);
     return 0;
 }
